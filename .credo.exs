6cd1198598d19f71d6212119e0c1025036077efc
Łukasz Niemier <~@hauleth.dev>
2025-06-20 23:32:13 +0200

    chore: update license in all modified files

--- .credo.exs
+++ .credo.exs
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2025 Supabase <support@supabase.io>
+# SPDX-FileCopyrightText: 2025 Łukasz Niemier <~@hauleth.dev>
 #
 # SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: EUPL-1.2
 
 # This file contains the configuration for Credo and you are probably reading
 # this after creating it with `mix credo.gen.config`.
--- test/integration/js/postgres/index.js
+++ test/integration/js/postgres/index.js
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2025 Supabase <support@supabase.io>
+// SPDX-FileCopyrightText: 2025 Łukasz Niemier <~@hauleth.dev>
 //
 // SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: EUPL-1.2
 
 import { t, nt, ot } from './test.js' // eslint-disable-line
 import net from 'node:net'
