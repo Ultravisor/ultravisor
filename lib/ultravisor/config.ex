89df3c34b35d9c8956d43d8e970a0595514f31e3
Łukasz Niemier <~@hauleth.dev>
2025-06-23 16:57:07 +0200

    docs: add documentation to Ultravisor.Config

--- lib/ultravisor/config.ex
+++ lib/ultravisor/config.ex
@@ -3,6 +3,18 @@
 # SPDX-License-Identifier: EUPL-1.2
 
 defmodule Ultravisor.Config do
+  @moduledoc """
+  Helper functions for fetching configuration options from system environment
+  """
+
+  @doc """
+  Read environment variable and try to cast it to boolean.
+
+  Values treated as `false` are `0` and `false`. Any other value (including
+  empty variable) is treated as `true`.
+  """
+  @spec get_bool(String.t()) :: boolean()
+  @spec get_bool(String.t(), boolean()) :: boolean()
   def get_bool(env_name, default \\ true) do
     case System.fetch_env(env_name) do
       {:ok, falsey} when falsey in ~w[0 false] -> false
@@ -11,6 +23,11 @@ defmodule Ultravisor.Config do
     end
   end
 
+  @doc """
+  Read environment variable and try to cast it to integer
+  """
+  @spec get_integer(String.t()) :: integer() | nil
+  @spec get_integer(String.t(), integer() | nil) :: integer() | nil
   def get_integer(env_name, default \\ nil) do
     case System.fetch_env(env_name) do
       {:ok, env} -> String.to_integer(env)
@@ -18,6 +35,11 @@ defmodule Ultravisor.Config do
     end
   end
 
+  @doc """
+  Read environment variable and try to cast it to one of the predefined atoms
+  """
+  @spec get_enum(String.t(), [atom()]) :: atom() | nil
+  @spec get_enum(String.t(), [atom()], atom() | nil) :: atom() | nil
   def get_enum(env_name, allowed_values, default \\ nil) do
     case System.fetch_env(env_name) do
       {:ok, env} ->
@@ -31,6 +53,13 @@ defmodule Ultravisor.Config do
     end
   end
 
+  @doc """
+  Read environment variable and split comma separated values from it
+
+  Omits empty values.
+  """
+  @spec get_list(String.t()) :: [String.t()]
+  @spec get_list(String.t(), [String.t()]) :: [String.t()]
   def get_list(env_name, default \\ []) do
     case System.fetch_env(env_name) do
       {:ok, env} -> String.split(env, ",", trim: true)
@@ -38,6 +67,11 @@ defmodule Ultravisor.Config do
     end
   end
 
+  @doc """
+  Read environment variable and try to extract JSON data stored in it
+  """
+  @spec get_json(String.t()) :: map()
+  @spec get_json(String.t(), map()) :: map()
   def get_json(env_name, default \\ %{}) do
     case System.fetch_env(env_name) do
       {:ok, env} -> JSON.decode!(env)
