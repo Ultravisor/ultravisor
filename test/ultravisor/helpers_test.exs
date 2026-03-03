562e122cb312b36f927a34baaab856486a9d71f2
Łukasz Niemier <~@hauleth.dev>
2026-03-03 21:22:37 +0100

    chore: use conventional name for name validation

--- test/ultravisor/helpers_test.exs
+++ test/ultravisor/helpers_test.exs
@@ -46,42 +46,40 @@ defmodule Ultravisor.HelpersTest do
     end
   end
 
-  describe "validate_name/1" do
+  describe "valid_name?/1" do
     test "Prisma migration databases are accepted" do
-      assert @subject.validate_name(
-               "prisma_migrate_shadow_db_dfe467a1-f7e4-4c27-87de-a930270f4622"
-             )
+      assert @subject.valid_name?("prisma_migrate_shadow_db_dfe467a1-f7e4-4c27-87de-a930270f4622")
     end
 
     property "ASCII strings with length within 1..63 are valid" do
-      check all name <- string(:ascii, min_length: 1, max_length: 63) do
-        assert @subject.validate_name(name)
+      check all(name <- string(:ascii, min_length: 1, max_length: 63)) do
+        assert @subject.valid_name?(name)
       end
     end
 
     property "string that is longer that 63 characters is invalid" do
-      check all name <- string(:printable, min_length: 64) do
-        refute @subject.validate_name(name)
+      check all(name <- string(:printable, min_length: 64)) do
+        refute @subject.valid_name?(name)
       end
     end
 
     property "printable strings with at most 63 *bytes* are valid" do
-      check all name <- string(:printable, min_length: 1, max_length: 63) do
+      check all(name <- string(:printable, min_length: 1, max_length: 63)) do
         # It is defined in weird way, as it is hard to generate strings with at
         # most 63 bytes, but that test is functionally equivalend
-        assert @subject.validate_name(name) == byte_size(name) < 64
+        assert @subject.valid_name?(name) == byte_size(name) < 64
       end
     end
 
     property "non-printable strings are invalid" do
-      check all prefix <- string(:utf8), suffix <- string(:utf8) do
-        refute @subject.validate_name(prefix <> <<0>>)
-        refute @subject.validate_name(<<0>> <> suffix)
-        refute @subject.validate_name(prefix <> <<0>> <> suffix)
+      check all(prefix <- string(:utf8), suffix <- string(:utf8)) do
+        refute @subject.valid_name?(prefix <> <<0>>)
+        refute @subject.valid_name?(<<0>> <> suffix)
+        refute @subject.valid_name?(prefix <> <<0>> <> suffix)
 
-        refute @subject.validate_name(prefix <> <<0x10>>)
-        refute @subject.validate_name(<<0x10>> <> suffix)
-        refute @subject.validate_name(prefix <> <<0x10>> <> suffix)
+        refute @subject.valid_name?(prefix <> <<0x10>>)
+        refute @subject.valid_name?(<<0x10>> <> suffix)
+        refute @subject.valid_name?(prefix <> <<0x10>> <> suffix)
       end
     end
   end
