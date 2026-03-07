d5ff1da090158eab7db309e98733ae5a25a26de1
Łukasz Niemier <~@hauleth.dev>
2026-03-07 13:57:16 +0100

    chore: use Devenv module instead of shell building function

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-License-Identifier: EUPL-1.2
-
 {
   description = "Elixir's application";
 
@@ -22,6 +21,10 @@
     ...
   } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        devenv.flakeModule
+      ];
+
       flake = {};
 
       systems = [
@@ -41,9 +44,6 @@
         formatter = pkgs.alejandra;
 
         packages = {
-          # Expose Devenv supervisor
-          devenv-up = self'.devShells.default.config.procfileScript;
-
           ultravisor = let
             erl = pkgs.beam_nox.packages.erlang_28;
           in
@@ -52,65 +52,45 @@
           default = self'.packages.ultravisor;
         };
 
-        devShells.default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-
-          modules = [
-            {
-              languages.elixir = {
-                enable = true;
-                package = pkgs.beam.packages.erlang_28.elixir_1_19;
-              };
-
-              # env.DYLD_INSERT_LIBRARIES = "${pkgs.mimalloc}/lib/libmimalloc.dylib";
-            }
-            {
-              packages = [
-                pkgs.pgbouncer
-                pkgs.pgdog
-                pkgs.xan
-              ];
-
-              services.postgres = {
-                enable = true;
-                package = pkgs.postgresql_18;
-                initialScript = ''
-                  ${builtins.readFile ./dev/postgres/00-setup.sql}
-
-                  CREATE USER postgres SUPERUSER PASSWORD 'postgres';
-                '';
-                listen_addresses = "127.0.0.1";
-                port = 6432;
-                settings = {
-                  max_prepared_transactions = 262143;
-                  random_page_cost = 1.1;
-                };
-              };
-
-              # Force connection through TCP instead of Unix socket
-              env.PGHOST = lib.mkForce "";
-
-              # env.DATABASE_URL = "postgres://postgres:postgres@localhost:6432/";
-            }
-            {
-              languages.javascript = {
-                enable = true;
-                bun.enable = true;
-                yarn.enable = true;
-              };
-            }
-            ({
-              pkgs,
-              ...
-            }: {
-              languages.cplusplus.enable = true;
-
-              packages =
-                [
-                  pkgs.prom2json
-                ];
-            })
+        devenv.shells.default = {
+          languages.elixir = {
+            enable = true;
+            package = pkgs.beam.packages.erlang_28.elixir_1_19;
+          };
+          packages = [
+            pkgs.pgbouncer
+            pkgs.pgdog
+            pkgs.xan
           ];
+
+          services.postgres = {
+            enable = true;
+            package = pkgs.postgresql_18;
+            initialScript = ''
+              ${builtins.readFile ./dev/postgres/00-setup.sql}
+
+              CREATE USER postgres SUPERUSER PASSWORD 'postgres';
+            '';
+            listen_addresses = "127.0.0.1";
+            port = 6432;
+            settings = {
+              max_prepared_transactions = 262143;
+              random_page_cost = 1.1;
+            };
+          };
+
+          process.manager.implementation = "honcho";
+
+          # Force connection through TCP instead of Unix socket
+          env.PGHOST = lib.mkForce "";
+
+          # env.DATABASE_URL = "postgres://postgres:postgres@localhost:6432/";
+
+          languages.javascript = {
+            enable = true;
+            bun.enable = true;
+            yarn.enable = true;
+          };
         };
       };
     };
